c958e4b7560318ddca53f32cf7f95f4ff4edc964
Karl Benbeneck <kbenbeneck@gmail.com>
2020-03-20 19:13:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age, COUNT(name) FROM bears WHERE gender = 'F' GROUP BY name;"
+  "SELECT name, age, COUNT(name) FROM bears WHERE gender == 'F' GROUP BY name;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
